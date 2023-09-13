f92236b16ac14434b1cefd3b6c650b8ab580a4fe
faithful <faithfulokondu@gmail.com>
2023-09-13 22:51:07 +0100

    name update

--- DROP-ANIM/src/components/Footer.jsx
+++ DROP-ANIM/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="container items-start mx-auto my-[100px] flex flex-col mdx:items-center">
       <div className="flex flex-col justify-between items-start my-6 w-[100%] mdx:flex-row mdx:items-center">
         <h1 className="text-grey font-bold text-3xl hover:text-white cursor-pointer my-3 md:my-0">
-          MECHANIC
+          AGROTECH
         </h1>
         <ul className="flex text-white flex-col sm:flex-row">
           <NavLink to='/' className={({isActive})=>`mr-6 hover:text-primary ${isActive ? 'text-primary' : 'text-white'} cursor-pointer mb-4 sideBar:mb-0`}>Home</NavLink>
